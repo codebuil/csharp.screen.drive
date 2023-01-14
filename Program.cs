ac1ab091b8ef7f3673634e82397b3321dd761f68
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-01-14 12:11:53 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,25 +9,35 @@ namespace screens
 {
     internal class Program
     {
+        static void scrollb8000()
+        {
+            Console.SetCursorPosition(0,29);
+            Console.WriteLine("");
+        }
         static void copyb8000(Int32 addresss,string s)
         {
             Int32 i = addresss / 2;
             int y = i / 80;
             int x = i - (y * 80);
-            Console.SetCursorPosition(x,y);
-            Console.Write(s);
+            if (y < 30)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(s);
+            }
         }
         static void mainss() {
             int x = 0;
             int y = 1;
             string memory = "hello world...";
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.Clear();
+            
             copyb8000(y*80*2+x, memory);
+            scrollb8000();
         }
         static void Main(string[] args)
 
         {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
             mainss();
 
 
